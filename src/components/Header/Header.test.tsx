b7ca7a4c3083c91d2156bb57fb37604fd1a925da
Alex Uribe <repzyyog@gmail.com>
2025-04-21 21:25:21 +0200

    Add test to Header component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Given the Header component", () => {
+  describe("When it renders", () => {
+    test("Then it should show 'Aliset comiendo por el mundo' inside a level 1 heading", () => {});
+    render(<Header />);
+
+    const appTitle = screen.getByRole("heading", {
+      name: /aliset comiendo por el mundo/i,
+      level: 1,
+    });
+
+    expect(appTitle).toBeVisible();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    root: "src",
+    globals: true,
+    reporters: [`verbose`],
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+    coverage: {
+      reportsDirectory: "../coverage",
+      exclude: ["main.tsx", "vite-env.d.ts", "**/types.ts"],
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["**/*.ts", "**/*.tsx"],
+    },
+  },
+
   plugins: [react()],
-})
+});
