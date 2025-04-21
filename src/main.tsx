21d0e11df38a651d0c347170f6cb0c378c5f08c3
Alex Uribe <repzyyog@gmail.com>
2025-04-21 21:29:43 +0200

    Fix import css order

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/styles.css";
 import App from "./components/App/App";
 import "@fontsource-variable/outfit/index.css";
+import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
