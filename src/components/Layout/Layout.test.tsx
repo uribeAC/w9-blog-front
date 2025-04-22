374c285ebe0737e3f951c087992b51838d170321
Ivet Lopez <ivet19@live.com>
2025-04-22 17:09:17 +0200

    Create Navigation test and update Header and Layout tests

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Layout from "./Layout";
+import { MemoryRouter } from "react-router";
 
 describe("Given the Header component", () => {
   describe("When it renders", () => {
     test("Then it should show 'Aliset comiendo por el mundo' inside a level 1 heading", () => {});
-    render(<Layout />);
+    render(
+      <MemoryRouter>
+        <Layout />
+      </MemoryRouter>,
+    );
 
     const appTitle = screen.getByRole("heading", {
       name: /aliset comiendo por el mundo/i,
