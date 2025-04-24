faaa591be9e5edc2298524dadd87979b3cd47e12
Ivet Lopez <ivet19@live.com>
2025-04-24 21:03:43 +0200

    Create PostDto interface

--- src/post/context/dto/types.ts
+++ src/post/context/dto/types.ts
@@ -1,6 +1,6 @@
-export interface Post {
-  id: string;
-  publishDate: string;
+export interface PostDto {
+  _id: string;
+  publishDate: Date;
   author: string;
   title: string;
   imageUrl: string;
