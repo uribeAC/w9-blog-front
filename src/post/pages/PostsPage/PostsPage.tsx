192b69238ff85354cdfde7572718b849a323fb01
Alex Uribe <repzyyog@gmail.com>
2025-04-24 09:00:00 +0200

    Add logic to page header counter

--- src/post/pages/PostsPage/PostsPage.tsx
+++ src/post/pages/PostsPage/PostsPage.tsx
@@ -5,12 +5,15 @@ import "./PostsPage.css";
 
 const PostsPage: React.FC = () => {
   const posts: Post[] = [];
+  const postsTotal: number = 30;
 
   return (
     <>
       <header className="page-header">
         <h2 className="page-header__title">Posts</h2>
-        <span className="page-header__counter">0 - 5 / 30</span>
+        <span className="page-header__counter">
+          {posts.length} / {postsTotal}
+        </span>
       </header>
       <PostsList posts={posts} />
     </>
