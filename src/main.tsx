58bcbbeef4d3d34ff6c9d0414fd4a024742dd6de
Alex Uribe <repzyyog@gmail.com>
2025-04-24 11:47:22 +0200

    Refactor duplicated PostsPage css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import AppRouter from "./router/AppRouter";
 import "@fontsource-variable/outfit/index.css";
 import "./styles/styles.css";
-import AppRouter from "./router/AppRouter";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
