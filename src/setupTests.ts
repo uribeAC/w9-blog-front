19e243649914d4927fc01c7e2dd4efb59256d9b8
Alex Uribe <repzyyog@gmail.com>
2025-04-25 19:25:53 +0200

    Create handlers to mock api

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom/vitest";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./post/mocks/node";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
