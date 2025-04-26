8212aba3d6cfd0d53d5a980bac7722c6acfdd6d1
Alex Uribe <repzyyog@gmail.com>
2025-04-26 15:53:03 +0200

    Create PostForm component with styles

---
+++ src/post/components/PostForm/PostForm.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "./PostForm.css";
+
+const PostForm: React.FC = () => {
+  return (
+    <form className="post-form">
+      <div className="post-form__groups">
+        <h3 className="form-text">Campos obligatorios:</h3>
+        <div className="post-form__group">
+          <label htmlFor="title" className="post-form__text">
+            Título:
+          </label>
+          <input
+            type="text"
+            id="title"
+            className="post-form__control"
+            required
+          />
+        </div>
+        <div className="post-form__group">
+          <label htmlFor="author" className="post-form__text">
+            Autor/a:
+          </label>
+          <input
+            type="text"
+            id="author"
+            className="post-form__control"
+            required
+          />
+        </div>
+        <div className="post-form__group">
+          <label htmlFor="imageUrl" className="post-form__text">
+            Imagen Url:
+          </label>
+          <input
+            type="url"
+            id="imageUrl"
+            className="post-form__control"
+            required
+          />
+        </div>
+        <div className="post-form__group">
+          <label htmlFor="content" className="post-form__text">
+            Contenido:
+          </label>
+          <textarea
+            rows={6}
+            id="content"
+            className="post-form__control"
+            required
+          />
+        </div>
+      </div>
+      <div className="post-form__groups">
+        <h3 className="form-text">Campos opcionales:</h3>
+        <div className="post-form__group">
+          <label htmlFor="imageAlt" className="post-form__text">
+            Descripción de imagen:
+          </label>
+          <input type="text" id="imageAlt" className="post-form__control" />
+        </div>
+        <div className="post-form__group">
+          <label htmlFor="tags" className="post-form__text">
+            Etiquetas:
+          </label>
+          <input type="text" id="tags" className="post-form__control" />
+        </div>
+        <div className="post-form__group">
+          <label htmlFor="publishDate" className="post-form__text">
+            Fecha de publicación:
+          </label>
+          <input type="date" id="publishDate" className="post-form__control" />
+        </div>
+      </div>
+      <button className="post-form__button" type="submit">
+        Crear post
+      </button>
+    </form>
+  );
+};
+
+export default PostForm;
