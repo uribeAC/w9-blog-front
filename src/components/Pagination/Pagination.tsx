05a8c74c8ef3d1a34dfa447b92d7fd4d746684cf
Isabel Sáenz <isasaete@gmail.comn>
2025-04-26 14:03:44 +0200

    Add Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router";
+import "./Pagination.css";
+
+interface PaginationProps {
+  postsTotal: number;
+  currentPage: number;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ postsTotal, currentPage }) => {
+  const pagesTotal = Math.ceil(postsTotal / 5);
+  const previousPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+
+  const firstPageClass = currentPage > 1 ? "" : " paginator__link--hidden";
+  const lastPageClass =
+    currentPage < pagesTotal ? "" : " paginator__link--hidden";
+
+  return (
+    <nav className="paginator">
+      <NavLink
+        className={`paginator__link ${firstPageClass}`}
+        to={`/posts/${previousPage}`}
+      >
+        {"<"}
+      </NavLink>
+      <span className="pagintator__current-page">{currentPage}</span>
+      <NavLink
+        className={`paginator__link ${lastPageClass}`}
+        to={`/posts/${nextPage}`}
+      >
+        {">"}
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Pagination;
