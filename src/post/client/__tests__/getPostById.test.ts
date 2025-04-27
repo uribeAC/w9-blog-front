91726b65058fb69bb4800b6b12c0789a0042177f
Ivet Lopez <ivet19@live.com>
2025-04-27 21:09:58 +0200

    Fix typo error in getPostById test

--- src/post/client/__tests__/getPostById.test.ts
+++ src/post/client/__tests__/getPostById.test.ts
@@ -2,7 +2,7 @@ import PostClient from "../PostClient";
 import { huevosRotosBruc159PostDto } from "../../dto/fixturesDto";
 import { mapPostDtoToPost } from "../../dto/mappers";
 
-describe("Given the deletePost method to PostClient", () => {
+describe("Given the getPostById method to PostClient", () => {
   describe("When it's called with 'Huevos rotos de Bruc, 159' id", () => {
     test("Then it should return Huevos rotos de Bruc, 159 post", async () => {
       const postClient = new PostClient();
