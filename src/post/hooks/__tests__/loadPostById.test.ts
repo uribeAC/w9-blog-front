08483be09f9cf8931c6233b4b84561d22008b45a
Alex Uribe <repzyyog@gmail.com>
2025-04-30 21:36:42 +0200

    Add test to loadPostById function from usePost

---
+++ src/post/hooks/__tests__/loadPostById.test.ts
@@ -0,0 +1,25 @@
+import { renderHook } from "@testing-library/react";
+import usePosts from "../usePosts";
+import { act } from "react";
+import { brochetasShinovarPostDto } from "../../dto/fixturesDto";
+
+describe("Given the loadPostById function", () => {
+  describe("When it's called with Brochetas de calma Shin 🌸🍢 post id", () => {
+    test("Then it should return Brochetas de calma Shin 🌸🍢 post", async () => {
+      const { result } = renderHook(() => usePosts());
+
+      await act(() => {
+        result.current.loadPostsByPage(2);
+        result.current.loadPostById(brochetasShinovarPostDto._id);
+      });
+
+      const posts = result.current.posts;
+
+      expect(posts).toContainEqual(
+        expect.objectContaining({
+          title: "Brochetas de calma Shin 🌸🍢",
+        }),
+      );
+    });
+  });
+});
