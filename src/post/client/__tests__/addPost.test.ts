6d2f148bb2616d6384b5ae8f2d7dde4d94d2e6ca
Alex Uribe <repzyyog@gmail.com>
2025-05-02 22:58:58 +0200

    Add throw error test to addPost method from PostClient

--- src/post/client/__tests__/addPost.test.ts
+++ src/post/client/__tests__/addPost.test.ts
@@ -2,6 +2,8 @@ import { huevosRotosBruc159PostDto } from "../../fixtures/fixturesDto";
 import { mapPostDtoToPost } from "../../dto/mappers";
 import { huevosRotosBruc159PostData } from "../../fixtures/fixtures";
 import PostClient from "../PostClient";
+import { server } from "../../mocks/node";
+import { http, HttpResponse } from "msw";
 
 describe("Given the addPost method to PostClient", () => {
   describe("When it's called with 'Huevos rotos de Bruc,159' data", () => {
@@ -13,6 +15,24 @@ describe("Given the addPost method to PostClient", () => {
       const postHuevos = mapPostDtoToPost(huevosRotosBruc159PostDto);
 
       expect(newPost).toStrictEqual(postHuevos);
+    });
+  });
+
+  describe("When it's called and response is not ok", () => {
+    test("Then it should throw 'Error adding new post'", async () => {
+      const apiUrl = import.meta.env.VITE_API_URL;
+
+      server.use(
+        http.post(`${apiUrl}/posts`, () => {
+          return new HttpResponse(null, { status: 500 });
+        }),
+      );
+
+      const postClient = new PostClient();
+
+      const newPost = postClient.addPost(huevosRotosBruc159PostData);
+
+      expect(newPost).rejects.toThrow("Error adding new post");
     });
   });
 });
