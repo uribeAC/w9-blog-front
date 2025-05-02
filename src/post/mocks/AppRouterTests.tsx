1148233b6767d33675e3df26329625bb133034c2
Alex Uribe <repzyyog@gmail.com>
2025-05-02 09:17:04 +0200

    Create AppRouter component mock

---
+++ src/post/mocks/AppRouterTests.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import PostsPage from "../pages/PostsPage/PostsPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import AddPostPage from "../pages/AddPostPage/AddPostPage";
+import PostDetailPage from "../pages/PostDetailPage/PostDetailPage";
+
+const AppRouterTests: React.FC = () => {
+  return (
+    <Routes>
+      <Route index element={<Navigate to="/posts" />} />
+      <Route path="/posts" element={<PostsPage />} />
+      <Route path="/add-post" element={<AddPostPage />} />
+      <Route path="/post/:id" element={<PostDetailPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
+
+export default AppRouterTests;
