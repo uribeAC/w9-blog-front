a9b6e4a2327f64c48a478bc7249b477321e718e2
Alex Uribe <repzyyog@gmail.com>
2025-05-02 19:17:11 +0200

    Move more info test to AppRouter component

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import userEvent from "@testing-library/user-event";
 import Layout from "./Layout";
 import PostsContextProvider from "../../post/context/PostsContextProvider";
 import AppTestRouter from "../../router/AppTestRouter";
-import { choutaKaladinPost } from "../../post/fixtures";
 
 const user = userEvent.setup();
 window.scrollTo = vitest.fn();
@@ -81,68 +80,6 @@ describe("Given the Layout component", () => {
         });
 
         expect(brochetasPostTitle).toBeVisible();
-      });
-    });
-
-    describe("And the user clicks the button '+ Info' del post Chouta callejero de Alethkar 🌯⚔️", () => {
-      test("Then it should show Chouta callejero de Alethkar 🌯⚔️ title post inside a heading", async () => {
-        render(
-          <PostsContextProvider>
-            <MemoryRouter initialEntries={["/posts"]}>
-              <Layout />
-              <AppTestRouter />
-            </MemoryRouter>
-          </PostsContextProvider>,
-        );
-
-        const choutaTitle = await screen.findByRole("heading", {
-          name: /chouta callejero de alethkar 🌯⚔️/i,
-        });
-
-        const choutaCard = choutaTitle.parentElement!.parentElement!;
-
-        const choutaMoreInfoButton = await within(choutaCard).findByRole(
-          "link",
-          { name: "+ info" },
-        );
-
-        await user.click(choutaMoreInfoButton);
-
-        const choutaPostTitle = await screen.findByRole("heading", {
-          name: /chouta callejero de alethkar 🌯⚔️/i,
-        });
-
-        expect(choutaPostTitle).toBeVisible();
-      });
-
-      test("Then it should show the full content of Chouta callejero de Alethkar 🌯⚔️ post", async () => {
-        render(
-          <PostsContextProvider>
-            <MemoryRouter initialEntries={["/posts"]}>
-              <Layout />
-              <AppTestRouter />
-            </MemoryRouter>
-          </PostsContextProvider>,
-        );
-
-        const choutaTitle = await screen.findByRole("heading", {
-          name: /chouta callejero de alethkar 🌯⚔️/i,
-        });
-
-        const choutaCard = choutaTitle.parentElement!.parentElement!;
-
-        const choutaMoreInfoButton = await within(choutaCard).findByRole(
-          "link",
-          { name: "+ info" },
-        );
-
-        await user.click(choutaMoreInfoButton);
-
-        const choutaPostContent = await screen.findByText(
-          choutaKaladinPost.content,
-        );
-
-        expect(choutaPostContent).toBeVisible();
       });
     });
 
---
+++ src/router/AppRouter.test.tsx
@@ -0,0 +1,69 @@
+import { render, screen, within } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import userEvent from "@testing-library/user-event";
+import PostsContextProvider from "../post/context/PostsContextProvider";
+import { choutaKaladinPost } from "../post/fixtures";
+import AppRouter from "./AppRouter";
+
+const user = userEvent.setup();
+window.scrollTo = vitest.fn();
+
+describe("Given the Layout component", () => {
+  describe("When it renders in path /posts and the user clicks the button '+ Info' del post Chouta callejero de Alethkar 🌯⚔️", () => {
+    test("Then it should show Chouta callejero de Alethkar 🌯⚔️ title post inside a heading", async () => {
+      render(
+        <PostsContextProvider>
+          <MemoryRouter initialEntries={["/posts"]}>
+            <AppRouter />
+          </MemoryRouter>
+        </PostsContextProvider>,
+      );
+
+      const choutaTitle = await screen.findByRole("heading", {
+        name: /chouta callejero de alethkar 🌯⚔️/i,
+      });
+
+      const choutaCard = choutaTitle.parentElement!.parentElement!;
+
+      const choutaMoreInfoButton = await within(choutaCard).findByRole("link", {
+        name: "+ info",
+      });
+
+      await user.click(choutaMoreInfoButton);
+
+      const choutaPostTitle = await screen.findByRole("heading", {
+        name: /chouta callejero de alethkar 🌯⚔️/i,
+      });
+
+      expect(choutaPostTitle).toBeVisible();
+    });
+
+    test("Then it should show the full content of Chouta callejero de Alethkar 🌯⚔️ post", async () => {
+      render(
+        <PostsContextProvider>
+          <MemoryRouter initialEntries={["/posts"]}>
+            <AppRouter />
+          </MemoryRouter>
+        </PostsContextProvider>,
+      );
+
+      const choutaTitle = await screen.findByRole("heading", {
+        name: /chouta callejero de alethkar 🌯⚔️/i,
+      });
+
+      const choutaCard = choutaTitle.parentElement!.parentElement!;
+
+      const choutaMoreInfoButton = await within(choutaCard).findByRole("link", {
+        name: "+ info",
+      });
+
+      await user.click(choutaMoreInfoButton);
+
+      const choutaPostContent = await screen.findByText(
+        choutaKaladinPost.content,
+      );
+
+      expect(choutaPostContent).toBeVisible();
+    });
+  });
+});
