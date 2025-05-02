1cc92b2dc3ec2b0ae1690908f23b9db6ef1cc25b
Alex Uribe <repzyyog@gmail.com>
2025-05-02 20:05:24 +0200

    Add smallImageUrl to PostDto

--- src/post/dto/types.ts
+++ src/post/dto/types.ts
@@ -2,10 +2,5 @@ import { Post } from "../types";
 
 export type PostDto = Omit<
   Post,
-  | "id"
-  | "smallImageUrl"
-  | "detailImageUrl"
-  | "previewTags"
-  | "previewContent"
-  | "publishDate"
+  "id" | "detailImageUrl" | "previewTags" | "previewContent" | "publishDate"
 > & { _id: string; publishDate: string };
