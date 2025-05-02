c2038e554ce49ae89625fd2a90550a6decc38c21
Alex Uribe <repzyyog@gmail.com>
2025-05-02 09:40:00 +0200

    Fix AppRouter mock

--- src/post/mocks/AppRouterMock.tsx
+++ src/post/mocks/AppRouterMock.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import PostsPage from "../pages/PostsPage/PostsPage";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 import AddPostPage from "../pages/AddPostPage/AddPostPage";
 import PostDetailPage from "../pages/PostDetailPage/PostDetailPage";
 
 const AppRouterMock: React.FC = () => {
   return (
     <Routes>
-      <Route index element={<Navigate to="/posts" />} />
       <Route path="/posts" element={<PostsPage />} />
       <Route path="/add-post" element={<AddPostPage />} />
       <Route path="/post/:id" element={<PostDetailPage />} />
-      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
