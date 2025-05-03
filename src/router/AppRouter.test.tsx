7d3be06ef5ec5be7af52fa48320f78409a64115b
Alex Uribe <repzyyog@gmail.com>
2025-05-03 22:42:52 +0200

    Add modeal create post AppRouter test

--- src/router/AppRouter.test.tsx
+++ src/router/AppRouter.test.tsx
@@ -9,88 +9,130 @@ const user = userEvent.setup();
 window.scrollTo = vitest.fn();
 
 describe("Given the AppRouter component", () => {
-  describe("When it renders in path /posts and the user clicks the button '+ Info' del post Chouta callejero de Alethkar 🌯⚔️", () => {
-    test("Then it should show Chouta callejero de Alethkar 🌯⚔️ title post inside a heading", async () => {
-      render(
-        <PostsContextProvider>
-          <MemoryRouter initialEntries={["/posts"]}>
-            <AppRouter />
-          </MemoryRouter>
-        </PostsContextProvider>,
-      );
+  describe("When it renders in path /posts", () => {
+    describe("And the user clicks the button '+ Info' del post Chouta callejero de Alethkar 🌯⚔️", () => {
+      test("Then it should show Chouta callejero de Alethkar 🌯⚔️ title post inside a heading", async () => {
+        render(
+          <PostsContextProvider>
+            <MemoryRouter initialEntries={["/posts"]}>
+              <AppRouter />
+            </MemoryRouter>
+          </PostsContextProvider>,
+        );
 
-      const choutaTitle = await screen.findByRole("heading", {
-        name: /chouta callejero de alethkar 🌯⚔️/i,
+        const choutaTitle = await screen.findByRole("heading", {
+          name: /chouta callejero de alethkar 🌯⚔️/i,
+        });
+
+        const choutaCard = choutaTitle.parentElement!.parentElement!;
+
+        const choutaMoreInfoButton = await within(choutaCard).findByRole(
+          "link",
+          {
+            name: "+ info",
+          },
+        );
+
+        await user.click(choutaMoreInfoButton);
+
+        const choutaPostTitle = await screen.findByRole("heading", {
+          name: /chouta callejero de alethkar 🌯⚔️/i,
+        });
+
+        expect(choutaPostTitle).toBeVisible();
       });
 
-      const choutaCard = choutaTitle.parentElement!.parentElement!;
+      test("Then it should show the full content of Chouta callejero de Alethkar 🌯⚔️ post", async () => {
+        render(
+          <PostsContextProvider>
+            <MemoryRouter initialEntries={["/posts"]}>
+              <AppRouter />
+            </MemoryRouter>
+          </PostsContextProvider>,
+        );
 
-      const choutaMoreInfoButton = await within(choutaCard).findByRole("link", {
-        name: "+ info",
+        const choutaTitle = await screen.findByRole("heading", {
+          name: /chouta callejero de alethkar 🌯⚔️/i,
+        });
+
+        const choutaCard = choutaTitle.parentElement!.parentElement!;
+
+        const choutaMoreInfoButton = await within(choutaCard).findByRole(
+          "link",
+          {
+            name: "+ info",
+          },
+        );
+
+        await user.click(choutaMoreInfoButton);
+
+        const choutaPostContent = await screen.findByText(
+          choutaKaladinPost.content,
+        );
+
+        expect(choutaPostContent).toBeVisible();
       });
-
-      await user.click(choutaMoreInfoButton);
-
-      const choutaPostTitle = await screen.findByRole("heading", {
-        name: /chouta callejero de alethkar 🌯⚔️/i,
-      });
-
-      expect(choutaPostTitle).toBeVisible();
     });
 
-    test("Then it should show the full content of Chouta callejero de Alethkar 🌯⚔️ post", async () => {
-      render(
-        <PostsContextProvider>
-          <MemoryRouter initialEntries={["/posts"]}>
+    describe("And the user clicks the button with label 'eliminar post' from Chouta callejero de Alethkar post", () => {
+      test("Then it should show 'El post ha sido eliminado' inside a heading", async () => {
+        const expectedPostTitle = /chouta callejero de alethkar 🌯⚔️/i;
+
+        render(
+          <PostsContextProvider>
             <AppRouter />
-          </MemoryRouter>
-        </PostsContextProvider>,
-      );
+          </PostsContextProvider>,
+          { wrapper: MemoryRouter },
+        );
 
-      const choutaTitle = await screen.findByRole("heading", {
-        name: /chouta callejero de alethkar 🌯⚔️/i,
+        const choutaTitle = await screen.findByRole("heading", {
+          name: expectedPostTitle,
+        });
+
+        const choutaCard = choutaTitle.parentElement!.parentElement!;
+
+        const deleteButton =
+          await within(choutaCard).findByLabelText(/eliminar post/i);
+
+        await user.click(deleteButton);
+
+        const modalText = await screen.queryByRole("heading", {
+          name: /el post ha sido eliminado/i,
+        });
+
+        expect(modalText).toBeVisible();
       });
-
-      const choutaCard = choutaTitle.parentElement!.parentElement!;
-
-      const choutaMoreInfoButton = await within(choutaCard).findByRole("link", {
-        name: "+ info",
-      });
-
-      await user.click(choutaMoreInfoButton);
-
-      const choutaPostContent = await screen.findByText(
-        choutaKaladinPost.content,
-      );
-
-      expect(choutaPostContent).toBeVisible();
     });
   });
 
-  describe("When it renders in path /posts and the user clicks the button with label 'eliminar post' from Chouta callejero de Alethkar post", () => {
-    test("Then it should show 'El post ha sido eliminado' inside a heading", async () => {
-      const expectedPostTitle = /chouta callejero de alethkar 🌯⚔️/i;
-
+  describe("When it renders in path /add-post and the user submits a post", () => {
+    test("Then it should show 'Post creado' inside a heading", async () => {
       render(
         <PostsContextProvider>
-          <AppRouter />
+          <MemoryRouter initialEntries={["/add-post"]}>
+            <AppRouter />
+          </MemoryRouter>
         </PostsContextProvider>,
-        { wrapper: MemoryRouter },
       );
 
-      const choutaTitle = await screen.findByRole("heading", {
-        name: expectedPostTitle,
+      const titleTextBox = screen.getByLabelText(/título/i);
+      const authorTextBox = screen.getByLabelText(/autor\/a/i);
+      const imageTextBox = screen.getByLabelText(/imagen url/i);
+      const contentTextBox = screen.getByLabelText(/contenido/i);
+
+      await user.type(titleTextBox, "Croquetas de la abuela");
+      await user.type(authorTextBox, "Alex");
+      await user.type(imageTextBox, "https://www.google.com/");
+      await user.type(contentTextBox, "Croquetillas");
+
+      const submitButton = screen.getByRole("button", {
+        name: /crear post/i,
       });
 
-      const choutaCard = choutaTitle.parentElement!.parentElement!;
-
-      const deleteButton =
-        await within(choutaCard).findByLabelText(/eliminar post/i);
-
-      await user.click(deleteButton);
+      await user.click(submitButton);
 
       const modalText = await screen.queryByRole("heading", {
-        name: /el post ha sido eliminado/i,
+        name: /post creado/i,
       });
 
       expect(modalText).toBeVisible();
